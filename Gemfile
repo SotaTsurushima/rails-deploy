124e406e5a73ac68eae4c8b9622737f52a17f326
SotaTsurushima <2018cassotatsurushima@gmail.com>
2022-12-28 17:16:08 +0000

    capistrano file 更新

--- Gemfile
+++ Gemfile
@@ -12,16 +12,13 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
-group :development, :test do
+group :development do
   gem 'capistrano'
   gem 'capistrano-rbenv'
   gem 'capistrano-bundler'
   gem 'capistrano-rails'
   gem 'capistrano3-unicorn'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-end
-
-group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.2'
   gem 'spring'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,36 @@
-# config valid for current version and patch releases of Capistrano
+# Capistranoのバージョンを指定（初期から記入済み）
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# アプリケーションの指定
+set :application, "rails-deploy"
+set :repo_url, "git@github.com:SotaTsurushima/rails-deploy.git"
+
+# sharedディレクトリに入れるファイルを指定
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+# SSH接続設定
+set :ssh_options, {
+  keys: ['~/.ssh/rails_deploy.pem'] 
+}
+
+# 保存しておく世代の設定
+set :keep_releases, 5
+
+# rbenvの設定
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.6'
+
+# ここからUnicornの設定
+# Unicornのプロセスの指定
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの指定
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+# Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
